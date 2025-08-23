7254494dbd95462df3176e88bbd70ad90e66e4e7
GGbond <z2621915516@163.com>
2025-08-23 21:52:53 +0800

    Update start-menu palette

--- include/heat_start_menu.h
+++ include/heat_start_menu.h
@@ -1,4 +1,5 @@
 void HeatStartMenu_Init(void);
 void GoToHandleInput(void);
-#define MENU_PAL_COUNT 4
+#define MENU_PAL_COUNT 40
 #define DEFAULT_START_MENU_PALETTE 0
+#define VAR_START_MENU_PALETTE VAR_UNUSED_0x4052
